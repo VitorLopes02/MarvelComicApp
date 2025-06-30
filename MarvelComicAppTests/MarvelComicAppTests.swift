d081752b3753ce7160fa2ea17048a1169cbc2087
Vitor Lopes <vitor.lopes@portoseguro.com.br>
2025-06-30 10:32:59 -0300

    Initial Commit

---
+++ MarvelComicAppTests/MarvelComicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicAppTests.swift
+//  MarvelComicAppTests
+//
+//  Created by VITOR ANDRADE LOPES on 30/06/25.
+//
+
+import Testing
+@testable import MarvelComicApp
+
+struct MarvelComicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
