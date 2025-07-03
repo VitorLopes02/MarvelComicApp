8224e497fe1e942ea4e7da1fad877d0cc6f86ef1
Vitor Lopes <vitinhoolopes.vl@gmail.com>
2025-07-03 09:36:42 -0300

    feat: add new xcconfig folder

--- MarvelComicApp/Core/Constants/Environment.swift
+++ MarvelComicApp/Core/Constants/Environment.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+enum Environment {
+    static var baseURL: String {
+        return Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String ?? ""
+    }
+    
+    static var publicKey: String {
+        return Bundle.main.object(forInfoDictionaryKey: "API_PUBLIC_KEY") as? String ?? ""
+    }
+    
+    static var privateKey: String {
+        return Bundle.main.object(forInfoDictionaryKey: "API_PRIVATE_KEY") as? String ?? ""
+    }
+}
